fa9f46560d60622b376ffe27424ae7191698b659
Anastasia <aveanip@yandex.ru>
2026-09-04 23:54:55 +0300

    added tests

--- src/test/java/specs/login/LoginSpec.java
+++ src/test/java/specs/login/LoginSpec.java
@@ -1,11 +1,9 @@
 package specs.login;
 
 import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import static io.restassured.RestAssured.with;
+
 import static io.restassured.filter.log.LogDetail.ALL;
-import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.notNullValue;
 
